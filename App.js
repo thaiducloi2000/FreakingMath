364c4319a5be778671ca9aa7bb765973027a1657
thaiducloi2000 <thaiducloi2000@gmail.com>
2022-02-25 16:22:27 +0700

    Add ReadMe.md

--- App.js
+++ App.js
@@ -1,20 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight, Alert, Dimensions, Modal, BackHandler } from 'react-native';
+import FlashScreen from './app/components/FlashScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class FreakingMath extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      score: 0,
+      math: '',
+      result: '',
+      result_show: '',
+      loop: [],
+      timeCounter: Dimensions.get('window').width,
+      width_screen: Dimensions.get('window').width,
+      modalVisible: true
+    }
+    this.initMath = this.initMath.bind(this);
+    this.stopLoop = this.stopLoop.bind(this);
+  } 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  initMath() {
+    this.stopLoop();
+    const list_oparetors_easy = ['+', '-','*','/'];
+    const one = Math.floor(Math.random() * 10);
+    const two = Math.floor(Math.random() * 10);
+    const operator = list_oparetors_easy[Math.floor(Math.random() * list_oparetors_easy.length)];
+    const result = eval('one ' + operator + ' two');
+    const result_show = eval('result '+ list_oparetors_easy[Math.floor(Math.random() * list_oparetors_easy.length)] + ' Math.floor(Math.random() * 2)');
+    this.setState({
+      math: '' + one + operator + two + ' = ' + result_show,
+      result: result,
+      result_show: result_show,
+      score: this.state.score += 1
+    });
+    this.countDown();
+  }
+
+  countDown() {
+    const loop = setInterval(() => {
+      if(this.refs.timeCounter){
+        this.refs.timeCounter.measure((ox, oy, width, height) => {
+          this.setState({timeCounter: width - this.state.width_screen/10});
+        });
+        if(this.state.timeCounter <= 0){
+          this.setState({timeCounter: Dimensions.get('window').width});
+          this.onTimeover();
+          this.stopLoop();
+          this.setState({score: -1});
+        };
+      }
+    }, 1000);
+    this.setState({loop: [...this.state.loop, loop]});
+  }
+
+  stopLoop(){
+    this.state.loop.forEach(function(loop){
+      clearInterval(loop);
+    });
+  }
+
+  onPressTouchableHighlight(boolean) {
+    this.setState({timeCounter: Dimensions.get('window').width});
+    this.stopLoop();
+    if((this.state.result == this.state.result_show) == boolean){
+      this.initMath();
+    } else {
+      this.onTimeover();
+      this.setState({score: 0});
+    } 
+  }
+
+  onTimeover() {
+    this.state.loop.forEach(function(loop){
+      clearInterval(loop);
+    });
+    Alert.alert('Opps',
+      'Gem over, score: ' + this.state.score,
+      [{text: 'Again'}]
+    );
+    this.initMath();
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <FlashScreen initMath={this.initMath}/>
+        <View style={{flex: 3, backgroundColor: 'powderblue'}}>
+          <View ref="timeCounter" style={{width: this.state.timeCounter,height: 35, marginTop: 50, backgroundColor: 'green'}}>
+            <Text style={{fontSize:30, color: 'white'}}>{this.state.score}</Text>
+          </View>
+          <View style={{height: 100, marginVertical: 180, marginHorizontal: 10}}>
+            <Text style={{fontSize:50, color: 'white', textAlign: 'center'}} ref="math">{this.state.math}</Text>
+          </View>
+        </View>
+        <View style={{flex: 1, backgroundColor: 'skyblue', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+          <TouchableHighlight
+            style={{flexGrow: 1, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.onPressTouchableHighlight(true)}>
+            <Text style={{fontSize:24, color: 'white'}}>True</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={{flexGrow: 1, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.onPressTouchableHighlight(false)}>
+            <Text style={{fontSize:24, color: 'white'}}>False</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# Welcome to Freaking Math Project
+### This repository contains a JavaScript code
+
+### You will find in this repo:
+* JavaScript code and root of React Native Project
+* .gitignore file to tell Git tool the things will be uploaded
+* README.md - this file itself to show the home-page of a repo
+### About Project:
+* Is a Basic Game base on Freaking Math in Play Store was by JavaScript.
+* Is the demo_1 Of Subject PRM301 seminar in FPT University
+* Project will be update every day
+* Project still in process.
+
+### Connect me via:
+* [Facebook](https://www.facebook.com/loi.thaiduc.2000/)
+* gmail : thaiducloi2000@gmail.com or loitdse140060@fpt.edu.vn(sub gmail)
+* Phone number : 0948260825
+#### © 2022 by Little Kid
